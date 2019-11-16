48a1ca315c4c9089281cd109e08f1277a17ed364
itiandong <j@itiandong.com>
2019-11-16 12:42:06 +0800

    调整搜索框布局

--- src/components/SearchTable/index.vue
+++ src/components/SearchTable/index.vue
@@ -6,19 +6,26 @@
           <el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="inputText"></el-input>
         </el-col>
         <el-col :span="3">
-          <el-button icon="el-icon-search" @click="()=>{this.searchText = inputText; this.currentPage = 1;}"></el-button>
+          <el-button
+            icon="el-icon-search"
+            @click="()=>{this.searchText = inputText; this.currentPage = 1;}"
+          ></el-button>
         </el-col>
-        <el-col :span="5" :offset="10" style="padding: 10px;font-size: 15px;color:#909399">
+        <el-col :span="7" :offset="8" style="padding: 10px;font-size: 15px;color:#909399">
           <strong>当前搜索:</strong>
-          <div style="border-bottom: solid 1px;display: inline-block;width:180px;text-align: center;">
-            {{searchText!=="" ? searchText:"未输入查询"}}
-          </div>
+          <div
+            style="border-bottom: solid 1px;display: inline-block;width:180px;text-align: center;"
+          >{{searchText!=="" ? searchText:"未输入查询"}}</div>
         </el-col>
       </el-row>
     </div>
     <el-row>
-      <el-table :data="filteredData.slice(this.currentPage*this.pageSize-this.pageSize, this.currentPage*this.pageSize)"
-                height="400" border style="width: 100%">
+      <el-table
+        :data="filteredData.slice(this.currentPage*this.pageSize-this.pageSize, this.currentPage*this.pageSize)"
+        height="400"
+        border
+        style="width: 100%"
+      >
         <el-table-column
           align="center"
           v-for="header in headerData"
@@ -37,49 +44,49 @@
         :total="total"
         :current-page="currentPage"
         @current-change="currentPageChange"
-        style="margin-top:10px;">
-      </el-pagination>
+        style="margin-top:10px;"
+      ></el-pagination>
     </el-row>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "SearchTable",
-        props: {
-            headerData: Array,
-            tableData: Array
-        },
-        computed: {
-            filteredData() {
-                let data = this.tableData.filter(item => {
-                    for (let header of this.headerData) {
-                        if (header.searchable === undefined || header.searchable === true) {
-                            if (String(item[header.prop]).indexOf(this.searchText) !== -1)
-                                return true;
-                        }
-                    }
-                    return false;
-                });
-                this.total = data.length;
-                return data;
-            }
-        },
-        data() {
-            return {
-                searchText: "",
-                inputText: "",
-                total: 0,
-                currentPage: 1,
-                pageSize: 7,
-            };
-        },
-        methods: {
-            currentPageChange(page) {
-                this.currentPage = page;
-            }
+export default {
+  name: "SearchTable",
+  props: {
+    headerData: Array,
+    tableData: Array
+  },
+  computed: {
+    filteredData() {
+      let data = this.tableData.filter(item => {
+        for (let header of this.headerData) {
+          if (header.searchable === undefined || header.searchable === true) {
+            if (String(item[header.prop]).indexOf(this.searchText) !== -1)
+              return true;
+          }
         }
+        return false;
+      });
+      this.total = data.length;
+      return data;
+    }
+  },
+  data() {
+    return {
+      searchText: "",
+      inputText: "",
+      total: 0,
+      currentPage: 1,
+      pageSize: 7
     };
+  },
+  methods: {
+    currentPageChange(page) {
+      this.currentPage = page;
+    }
+  }
+};
 </script>
 
 <style scoped>
